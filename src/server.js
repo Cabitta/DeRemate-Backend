c695b22aae02c8ae1de91ee89382632ba86d6e5d
Lucas <lucaspistolesi07@gmail.com>
2025-04-15 18:31:29 -0300

    created database schemas

--- src/server.js
+++ src/server.js
@@ -4,6 +4,11 @@ import cors from "cors";
 import { envConfig } from "./utils/envConfig.js";
 import { connectToMongoDB } from "./DB/dbConnection.js";
 
+import Client from './models/Client.js';
+import Delivery from './models/Delivery.js';
+import Package from './models/Package.js';
+import Route from './models/Route.js';
+
 const app = express();
 const PORT = envConfig.PORT || 3000;
 
@@ -18,12 +23,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+
 app.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
+app.get("api/delivery-list", (req, res) => {
+  res.status(200).json({
+    message: "Hello from the server!",
+  });
 });
