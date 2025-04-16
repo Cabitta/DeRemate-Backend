dd0062b2e49c414219f22a7f2c8158d06952db85
Lucas Pistolesi <84645028+lPisto@users.noreply.github.com>
2025-04-16 13:10:54 -0300

    Revert "Initial Commit"

--- src/routes/deliveryHistory.routes.js
+++ src/routes/deliveryHistory.routes.js
@@ -1,19 +1,19 @@
 import express from "express";
+import Route from "../models/route.js";
 import routeMapper from "../mappers/routeMapper.js";
-import { Client } from "../models/client.model.js";
-import { Route } from "../models/route.model.js";
+import Client from "../models/client.js";
 
 const router = express.Router();
 
 router.get("/delivery-history-list", async (req, res) => {
-  try {
-    const routes = await Route.find({ state: "delivered" }).populate("client");
-    const dtos = routes.map(routeMapper);
+    try {
+        const routes = await Route.find({ state: 'delivered' }).populate("client");
+        const dtos = routes.map(routeMapper);
 
-    res.json(dtos);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+        res.json(dtos);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 export default router;
