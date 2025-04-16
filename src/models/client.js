0471caa784226c4b1f387acbd691459d8b5a5227
martinbejarano <martinbejarano12@outlook.com>
2025-04-15 23:54:49 -0300

    Solucion conflictos merge

--- src/models/client.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const clientSchema = new mongoose.Schema({
-  firstname: String,
-  lastname: String,
-  email: String
-});
-
-export default mongoose.model('Client', clientSchema);
