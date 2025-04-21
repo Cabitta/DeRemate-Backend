3a50126a95637ad9dfae68f0ac0c1795d248e878
Franco <fr.cabanillas7@gmail.com>
2025-04-21 19:34:57 -0300

    controller availale routes

---
+++ src/controllers/availableRoutesController.js
@@ -0,0 +1,36 @@
+import Route from "../models/route.js";
+import { availableRouteMapper } from "../mappers/routeMapper.js";
+import "../models/package.js"
+
+export const getAvailableRoutesByDeliveryId = async (req, res) => {
+  try {
+    const { deliveryId } = req.query;
+    
+    // Validate deliveryId
+    if (!deliveryId) {
+      return res.status(400).json({ message: "Delivery ID is required" });
+    }
+    
+    // Fetch available routes for the given deliveryId
+    const routes = await Route.find({ delivery: deliveryId, state: "pending" })
+      .populate("client")
+      .populate("package");
+
+    // Check if routes exist
+    if (!routes || routes.length === 0) {
+      return res.status(404).json({ message: "No available routes found" });
+    }
+    
+    // Map the routes to DTOs
+    const dtos = routes.map(availableRouteMapper);
+
+    res.json(dtos);
+    
+  } catch (error) {
+    console.error("Error fetching available routes:", error);
+    res.status(500).json({
+      error: "An error occurred while fetching available routes.",
+    });
+    
+  }
+}
