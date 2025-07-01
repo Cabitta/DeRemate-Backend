1af07a244fa18d83f1197a8ba1e3193f04e878de
Rodrigo Moens <rodrigo_moens@hotmail.com>
2025-06-30 21:18:32 -0300

    Generacion de Codigos QRs

--- src/server.js
+++ src/server.js
@@ -5,16 +5,14 @@ import { envConfig } from "./utils/envConfig.js";
 import { connectToMongoDB } from "./DB/dbConnection.js";
 import deliveryHistoryRoutes from "./routes/deliveryHistory.routes.js";
 import authRoutes from "./routes/auth.routes.js";
+import qrRoutes from "./routes/qrRoutes.js"
 import availableRoutes from "./routes/availableRoutes.js";
 import swaggerUI from "swagger-ui-express";
 import { swaggerSpec } from "./docs/swagger.js";
 import passport from "passport";
 import { configurePassport } from "./passport.config.js";
-
 const app = express();
 const PORT = envConfig.PORT || 3000;
-
-//app.use(cors()); // Menos seguro, pero útil para desarrollo
 
 app.use(
   cors({
@@ -28,7 +26,6 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({origin: '*', credentials: true}));
 // Configurar Passport
 const configuredPassport = configurePassport();
 app.use(configuredPassport.initialize());
@@ -39,8 +36,9 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 app.use("/api", deliveryHistoryRoutes);
 app.use("/api", authRoutes);
 app.use("/api", availableRoutes);
+app.use("/api", qrRoutes)
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   connectToMongoDB();
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
